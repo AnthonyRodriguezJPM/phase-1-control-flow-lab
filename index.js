01cb4fc410e717186038673392ee65a82eae4f6c
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-08-10 01:53:44 -0700

    commit tree 526d87fc2a9efb3be9f97db86299cc2e31ce2acf

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+  if(feet<=400){
+    return 'This one is on me!';
+  }
+  else if (feet>=400 && feet<=2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (feet>2000 && feet<=2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(NYC){
+  return (NYC==='NYC') ? 'Ok, sounds good.' : 'No go.';
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
+  if(Tip==='generous') {
+    return "Thank you so much.";
+  }
+  else if (Tip==='not as generous') {
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye.';
+  }
   // Write your code here!
 }
